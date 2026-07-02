b84c1af007f6266ecbd284b5dbb88645a236ffdf
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-07-03 01:03:52 +0900

    Fix snap by findSectionOn

--- src/view/player-overlay.tsx
+++ src/view/player-overlay.tsx
@@ -5,34 +5,26 @@ import type { Track } from "../model/project.js";
 import { toStride, type QuantizeMode } from "../model/quantize.js";
 import type { SectionLine, TempoChange } from "../model/ruler-mark.js";
 import type { MicroSecond, Tempo, TickResolution } from "../model/time.js";
-import { MICROSECOND_PER_X, X_PER_SAMPLE } from "./track-list/waveform-def.js";
+import { MICROSECOND_PER_X } from "./track-list/waveform-def.js";
 
-function findNearest(microSeconds: MicroSecond, sectionLines: readonly SectionLine[]): SectionLine {
-    // search item that minimizes distance by ternary search method in O(log N)
-    const distance = (item: MicroSecond) => Math.abs(microSeconds - item);
+function findSectionOn(
+    microSeconds: MicroSecond,
+    sectionLines: readonly SectionLine[],
+): SectionLine {
+    // search section laying `microSeconds` by binary search method in O(log N)
     let start = 0;
-    let end = sectionLines.length - 1;
-    // the solution in [start, end]
-    while (start + 2 < end) {
-        const midLow = Math.floor((start * 2 + end) / 3);
-        const midHigh = Math.floor((start + end * 2) / 3);
-        if (distance(sectionLines[midLow]!.at) < distance(sectionLines[midHigh]!.at)) {
-            end = midHigh;
+    let end = sectionLines.length;
+    // the solution in [start, end)
+    while (start + 1 < end) {
+        const mid = start + Math.floor((end - start) / 2);
+        if (sectionLines[mid]!.at <= microSeconds) {
+            start = mid;
         } else {
-            start = midLow;
+            end = mid;
         }
     }
 
-    let minDistance = Infinity;
-    let minIndex = start;
-    for (let i = start; i <= end; ++i) {
-        const dist = distance(sectionLines[i]!.at);
-        if (dist < minDistance) {
-            minDistance = dist;
-            minIndex = i;
-        }
-    }
-    return sectionLines[minIndex]!;
+    return sectionLines[start]!;
 }
 
 function findTempoAt(microSeconds: MicroSecond, tempoChanges: readonly TempoChange[]): Tempo {
@@ -83,14 +75,13 @@ export const PlayerOverlay = ({
             const markerX = e.clientX - left;
 
             const markerMs = ((markerX * MICROSECOND_PER_X) / xScale) as MicroSecond;
-            const nearestSection = findNearest(markerMs, sectionLines);
-            const nearestSectionX = (nearestSection.at * xScale) / MICROSECOND_PER_X;
+            const sectionOn = findSectionOn(markerMs, sectionLines);
+            const sectionX = (sectionOn.at * xScale) / MICROSECOND_PER_X;
 
             const tempo = findTempoAt(markerMs, tempoChanges);
             const quantum = toStride(quantizeMode, resolution);
             const strideX = (quantum * tempo * xScale) / resolution / MICROSECOND_PER_X;
-            const snapX =
-                Math.round((markerX - nearestSectionX) / strideX) * strideX + nearestSectionX;
+            const snapX = Math.round((markerX - sectionX) / strideX) * strideX + sectionX;
             markerRef.current.style.transform = `translateX(${snapX}px)`;
         };
         divRef.current.addEventListener("mousemove", onMouseMove);
@@ -105,13 +96,16 @@ export const PlayerOverlay = ({
         }
 
         const { left } = divRef.current.getBoundingClientRect();
-        const clickMs = (((e.clientX - left) * MICROSECOND_PER_X) / xScale) as MicroSecond;
-        const nearestSection = findNearest(clickMs, sectionLines);
+        const clickX = e.clientX - left;
+        const clickMs = ((clickX * MICROSECOND_PER_X) / xScale) as MicroSecond;
+        const sectionOn = findSectionOn(clickMs, sectionLines);
+        const sectionMs = sectionOn.at;
+
         const tempo = findTempoAt(clickMs, tempoChanges);
         const quantum = toStride(quantizeMode, resolution);
         const quantumMs = (quantum * tempo * xScale) / resolution;
-        const snapMs = (Math.round((clickMs - nearestSection.at) / quantumMs) * quantumMs +
-            nearestSection.at) as MicroSecond;
+        const snapMs = (Math.round((clickMs - sectionMs) / quantumMs) * quantumMs +
+            sectionMs) as MicroSecond;
         dispatch({
             type: "TOGGLE_SLICE_MARK",
             at: snapMs,
